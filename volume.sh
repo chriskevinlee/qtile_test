4d9ab2a42f8bc5d76308cf0e6645d9cdc3d9184e
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-05-01 13:27:21 +0100

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -8,14 +8,8 @@ if [ "$1" = "up" ]; then
     pactl set-sink-volume @DEFAULT_SINK@ "$volume_increase"
 elif [ "$1" = "down" ]; then
     pactl set-sink-volume @DEFAULT_SINK@ "$volume_decrease"
-elif [ "$1" = "mute" ]; then
-    pactl set-sink-mute @DEFAULT_SINK@ "$volume_mute"
 fi
 
-is_muted=$(pactl list sinks | awk '/^\s*Mute:/ {print $2}')
-if [ "$is_muted" = "yes" ]; then
-    echo "Volume is muted"
-else
-    current_volume=$(pactl list sinks | awk '/^\s*Volume:/ {print $5}')
-    echo "Current volume level: $current_volume"
-fi
+current_volume=$(pactl list sinks | awk '/^\s*Volume:/ {print $5}')
+echo "Current volume level: $current_volume"
+
