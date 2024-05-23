4598bcca4048aa0a6f5c810f166d7626309a5bb2
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-05-23 14:45:45 +0100

    Update autostart.sh

--- myconfigs/scripts/autostart.sh
+++ myconfigs/scripts/autostart.sh
@@ -2,6 +2,6 @@
 xrandr -s 1920x1080 &
 # nitrogen --restore &
 feh --bg-scale .config/wallpapers/wallpaper_cave_nature/Sunset/32aGXc2-sunset-backgrounds.jpg
-mpv --no-video /home/chris/Downloads/computer-startup-music-97699.mp3
+mpv --no-video .config/sounds/startup-87026.mp3
 xscreensaver -no-splash &
 conky -c ~/.config/conky/conky.conf
